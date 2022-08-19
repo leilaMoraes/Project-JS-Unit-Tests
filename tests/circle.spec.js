ddee2d4f40b06a1a888cbee5158bad064728ec88
Leila Moraes <leila.violino@gmail.com>
2022-08-19 02:49:47 -0300

    Resolução do requisito 4

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -13,24 +13,42 @@ const circle = require('../src/circle');
   Comportamento:
     - circle(1) // Retorno: {radius: 1, area: 3.14, circumference: 6.28}
     - circle(7) // Retorno: {radius: 7, area: 153.86, circumference: 43.96}
-    - circle(3) // Retorno: {radius: 3, area: 28,26, circumference: 18.84}
+    - circle(3) // Retorno: {radius: 3, area: 28.26, circumference: 18.84}
 
   DICA: Números de ponto flutuante em JavaScript são imprecisos!  Para testar, vá no console do navegador e faça `0.2 + 0.1`.
-        Uma solução pra isso pode ser fazer a soma no seguinte formato: `parseFloat((0.2 + 0.1).toPrecision(2))`.
-        Outra dica: que tal pesquisar se existe um matcher que compara valores próximos?
-        Use esse conhecimento para te ajudar a lidar com possíveis problemas que esses testes trarão!
-*/
-
-describe('4 - Implemente os casos de teste para a função `circle`', () => {
-  it('Verifica se ao receber um raio, a função `circle` retorna um objeto contendo os valores esperados', () => {
-    fail('Teste vazio!');
-    // ESCREVA SEUS TESTES ABAIXO:
-    // Teste se circle retorna undefined, caso o parâmetro passado não seja um número.
-    // Teste se circle retorna um objeto.
-    // Teste se o objeto retornado possui 3 propriedades.
-    // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
-    // Teste se dentro do objeto retornado, a função retorna uma `key` com `value` igual à circunferência correta para um círculo de raio 2.
-    // Teste se dentro do objeto retornado, a função retorna uma `key` com `value` igual à área correta para um círculo de raio 3.
-    // Teste se a função retorna, em um objeto, os dados corretos de um círculo de raio 3.
+  Uma solução pra isso pode ser fazer a soma no seguinte formato: `parseFloat((0.2 + 0.1).toPrecision(2))`.
+  Outra dica: que tal pesquisar se existe um matcher que compara valores próximos?
+  Use esse conhecimento para te ajudar a lidar com possíveis problemas que esses testes trarão!
+  */
+ 
+ describe('4 - Implemente os casos de teste para a função `circle`', () => {
+  it('Verifica se ao receber um raio 3, a função `circle` retorna um objeto contendo os valores esperados', () => {
+    expect(circle(3)).toEqual({radius: 3, area: 28.259999999999998, circumference: 18.84});
   });
+  
+  it('Verifica se a função `circle` retorna `undefined` quando recebe valores que não sejam números', () => {
+    expect(circle(' ')).toBeUndefined();
+    expect(circle('a')).toBeUndefined();
+    expect(circle({ })).toBeUndefined();
+  });
+
+  it('Verifica se a função `circle` retorna um objeto com 3 propriedades', () => {
+    expect(circle(5)).toHaveProperty('radius', 'area', 'circumference');
+  });
+
+  it('Verifica se a função `circle` retorna `undefined` quando não recebe nenhum parâmetro', () => {
+    expect(circle()).toBeUndefined();
+  });
+  
+  it('Verifica se a função `circle` retorna uma `key` com `value` igual à circunferência correta para um círculo de raio 2', () => {
+    expect(circle(2).circumference).toEqual(12.56);
+  });
+  
+  it('Verifica se a função `circle` retorna uma `key` com `value` igual à area correta para um círculo de raio 3', () => {
+    expect(circle(3).area).toBeCloseTo(28.26);
+  });
+   
+  it('Verifica se a função `circle` retorna um objeto', () => {
+    expect(typeof circle(4)).toBe('object');
+  }); 
 });
