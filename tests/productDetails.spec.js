4afea43064876c4cbcae0b7e2384e6495a196c0d
Leila Moraes <leila.violino@gmail.com>
2022-08-21 00:22:04 -0300

    Resolução do requisito 6

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -31,13 +31,34 @@ const productDetails = require('../src/productDetails');
 
 describe('6 - Implemente os casos de teste para a função `productDetails`', () => {
   it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
-    fail('Teste vazio!');
-    // ESCREVA SEUS TESTES ABAIXO:
-    // Teste se productDetails é uma função.
-    // Teste se o retorno da função é um array.
-    // Teste se o array retornado pela função contém dois itens dentro.
-    // Teste se os dois itens dentro do array retornado pela função são objetos.
-    // Teste se quando passado parâmetros diferentes entre si, os dois objetos também são diferentes entre si.
-    // Teste se os dois productIds terminam com 123.
+    expect(productDetails('Alcool gel', 'Máscara')).toEqual([{name: 'Alcool gel', details: {productId: 'Alcool gel123'}},{name: 'Máscara', details: {productId: 'Máscara123'}}]);
+  });
+  
+  it('Verifica se a função `productDetails` é uma função', () => {
+    expect(typeof productDetails).toBe('function');
+  });
+
+  it('Verifica se o retorno da função `productDetails` é um array', () => {
+    expect(Array.isArray(productDetails('Alcool gel', 'Máscara'))).toBeTruthy();
+  });
+
+  it('Verifica se o array retornado pela função `productDetails` contém dois itens dentro', () => {
+    expect(productDetails('Alcool gel', 'Máscara').length).toEqual(2);
+  });
+
+  it('Verifica se os dois itens dentro do array retornado da função `productDetails` são um objeto', () => {
+    expect(typeof productDetails('Alcool gel', 'Máscara')[0]).toBe('object');
+    expect(typeof productDetails('Alcool gel', 'Máscara')[1]).toBe('object');
+  });
+
+  it('Verifica se quando passado parâmetros diferentes entre si na função `productDetails`, os dois objetos também são diferentes entre si', () => {
+    expect(productDetails('Celular', 'Carregador')[0]).not.toBe(productDetails('Celular', 'Carregador')[1])
+  });
+
+  it('Verifica se os dois productIds terminam com 123', () => {
+    const firstProduct = productDetails('Tênis', 'Meia')[0].details.productId;
+    const secondProduct = productDetails('Tênis', 'Meia')[1].details.productId;
+    expect(firstProduct.endsWith('123')).toBeTruthy();
+    expect(secondProduct.endsWith('123')).toBeTruthy();
   });
 });
